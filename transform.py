3a1aff1c5085472fa1b0e122dd77803df498dc02
HarshavardhanBollina <harshavbollina@gmail.com>
2024-03-14 12:48:06 -0500

    basic transformation

---
+++ transform.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("streaming_data.csv")
+
+df['time'] = pd.to_datetime(df['timestamp']).dt.time
+df['day'] = pd.to_datetime(df['timestamp']).dt.day_name()
+
+df = df.drop('timestamp', axis=1)
+# Ensure these headers match your CSV file exactly
+df.columns = ['City', 'Temperature', 'Day', 'Time'] 
+
+df
